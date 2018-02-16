c126c8e745718d8945e7fd0f5687af16cc53b364
haruelrovix <havitchoirul.rovik@mitrais.com>
2018-02-16 07:13:01 +0700

    RunGuest

--- lib/main.go
+++ lib/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 const port = "8080"
@@ -37,5 +38,16 @@ func RunHost(ip string) {
 // RunGuest takes destination IP as argument
 // and connects to that IP
 func RunGuest(ip string) {
-
+	ipAndPort := ip + ":" + port
+	conn, dialErr := net.Dial("tcp", ipAndPort)
+	if dialErr != nil {
+		log.Fatal("Error", dialErr)
+	}
+	fmt.Print("Send message: ")
+	reader := bufio.NewReader(os.Stdin)
+	message, readErr := reader.ReadString('\n')
+	if readErr != nil {
+		log.Fatal("Error", readErr)
+	}
+	fmt.Fprint(conn, message)
 }
