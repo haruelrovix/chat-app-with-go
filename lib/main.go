6c4ff0357812928a598b2f973247e02565b0746d
haruelrovix <havitchoirul.rovik@mitrais.com>
2018-02-16 07:20:10 +0700

    Long lived connection for the Host

--- lib/main.go
+++ lib/main.go
@@ -26,13 +26,26 @@ func RunHost(ip string) {
 	}
 	fmt.Println("New connections accepted")
 
+	for {
+		handleHost(conn)
+	}
+}
+
+func handleHost(conn net.Conn) {
 	reader := bufio.NewReader(conn)
 	message, readErr := reader.ReadString('\n')
 	if readErr != nil {
 		log.Fatal("Error", readErr)
 	}
-
 	fmt.Println("Message received: ", message)
+
+	fmt.Print("Send message: ")
+	replyReader := bufio.NewReader(os.Stdin)
+	replyMessage, replyErr := replyReader.ReadString('\n')
+	if replyErr != nil {
+		log.Fatal("Error", replyErr)
+	}
+	fmt.Fprint(conn, replyMessage)
 }
 
 // RunGuest takes destination IP as argument
